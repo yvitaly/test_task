a0e09f1aef8dc7059487b5270bfafa467b4c3082
yvitaly <yvitalyyy@gmail.com>
2021-08-04 15:49:23 +0300

    v5
    Додання помилки і переадресації у випадку неможливої авторизації

--- auth.php
+++ auth.php
@@ -5,12 +5,12 @@ $password = filter_var(trim($_POST['password']),FILTER_SANITIZE_STRING);
 $userPDO = 'root';
 $passwordPDO = '';
 $dbh = new PDO('mysql:host=localhost;dbname=ekreative-db', $userPDO, $passwordPDO);
-// здесь мы каким-то образом используем соединение
 $result = $dbh->query(" SELECT * FROM `users` WHERE `email` = '$email' and password = '$password'");
-//var_dump($result->fetch_assoc());die;
+
 $users = $result->fetchAll();
 if(empty($users)) {
-    echo(' polzovalel ne naiden');
+    header('Location: index.php');
+    $_SESSION['errors'] = ['No user found'];
     exit();
 }
 $user = $users[0];
--- index.php
+++ index.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+</head>
+<body>
 <div class="conreiner d-flex justify-content-center mt-4">
   <div>
     <button class="btn btn-primary" data-toggle="collapse" data-target="#demo">Regisration</button>
@@ -24,7 +25,11 @@
   </div>
   <div>
     <button class="btn btn-primary" data-toggle="collapse" data-target="#demo2">Login</button>
-
+      <?php if (!empty($_SESSION['errors'])): ?>
+          <?php foreach($_SESSION['errors'] as $error): ?>
+              <p><?php echo $error; ?></p>
+          <?php endforeach;  ?>
+      <?php endif; ?>
     <div id="demo2" class="collapse">
       <div class="mb-3">
         <form action="auth.php" method="post" >
@@ -43,5 +48,7 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 
-</head>
-<body>
+
+
+</body>
+</html>
